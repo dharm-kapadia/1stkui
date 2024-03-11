33a144463855bcb840c7fb79efe94a85b7381656
Dharm Kapadia <dharm.kapadia@equilend.com>
2024-03-11 15:43:49 -0400

    Remove APEX Charts from package.json

---
+++ src/utils/splits-mock-data.jsx
@@ -0,0 +1,36 @@
+import { Chance } from 'chance';
+
+const chance = new Chance();
+
+export const range = (len) => {
+  const arr = [];
+  for (let i = 0; i < len; i += 1) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+function mockSplitsData(index) {
+  return {
+    contractId: `${chance.natural({ min: 1, max: 999999999 })}${index}`,
+    processingStatus: chance.string(),
+    matchingSpirePositionId: chance.natural({ min: 1, max: 999999999 }),
+    contractStatus: chance.string(),
+    lastUpdateParty: chance.string(),
+    lastUpdateDateTime: chance.date,
+    venueRefKey: chance.string(),
+    cusip: chance.string(),
+    isin: chance.string(),
+    sedol: chance.string(),
+    ticker: chance.string(),
+    tradeDate: chance.date(),
+    settlementDate: chance.date(),
+    collateralType: chance.string(),
+    ccurrency: chance.string(),
+    internalPartyId: chance.string(),
+    accountId: chance.string(),
+    internalRefId: chance.string()
+  };
+}
+
+export default mockSplitsData;
