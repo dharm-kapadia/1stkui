a0348dffdcd47e28a7c3a7081563c64742c08573
Dharm Kapadia <dharm.kapadia@equilend.com>
2024-03-12 14:15:39 -0400

    Delete unnecessary mock data files

--- src/data/recalls-table.jsx
+++ src/data/recalls-table.jsx
@@ -1,4 +1,28 @@
-import mockRecallsData, { range } from 'utils/recalls-mock-data';
+import { Chance } from 'chance';
+
+const chance = new Chance();
+
+export const range = (len) => {
+  const arr = [];
+  for (let i = 0; i < len; i += 1) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+function mockRecallsData(index) {
+  return {
+    type: `${chance.natural({ min: 1, max: 999999999 })}${index}`,
+    source: chance.string(),
+    subject: chance.string(),
+    id: chance.natural({ min: 1, max: 999999999 }),
+    time: chance.date(),
+    relatedProcess: chance.string(),
+    relatedSubProcess: chance.string(),
+    relatedObjectId: chance.natural({ min: 1, max: 999999999 }),
+    relatedObjectType: chance.string()
+  };
+}
 
 const newRecall = (index) => {
   const tempData = mockRecallsData(index);
