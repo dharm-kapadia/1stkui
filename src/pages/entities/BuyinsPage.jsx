c31179a1bfdf5dd7df4567927cd007954ee89d90
Dharm Kapadia <dharm.kapadia@equilend.com>
2024-04-10 13:54:21 -0400

    Replace MUI Table with DataGrid on pages and set initial page size

--- src/pages/entities/BuyinsPage.jsx
+++ src/pages/entities/BuyinsPage.jsx
@@ -1,225 +1,84 @@
+import { Toolbar, Typography } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 import axios from 'axios';
 import PropTypes from 'prop-types';
-import { useEffect, useMemo, useState } from 'react';
-
-// material-ui
-import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
-import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
-
-// third-party
-import {
-  flexRender,
-  getCoreRowModel,
-  getFacetedMinMaxValues,
-  getFacetedRowModel,
-  getFacetedUniqueValues,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  useReactTable
-} from '@tanstack/react-table';
+import { useEffect, useState } from 'react';
 
 // project import
 import MainCard from 'components/MainCard';
-import { EmptyTable, Filter } from 'components/third-party/react-table';
+import { filterForDiscrepancies } from 'utils/jsonHelper';
 
-// ==============================|| REACT TABLE ||============================== //
+const columns = [
+  { field: 'buyinInitiatedId', headerName: 'Buyin Id', width: 175 },
+  { field: 'buyinDate', headerName: 'Buyin Date', width: 150 },
+  { field: 'contractId', headerName: 'Contract Id', width: 175 },
+  { field: 'status', headerName: 'Status', width: 150 },
+  { field: 'openQuantity', headerName: 'Open Qty', width: 150 },
+  { field: 'quantity', headerName: 'Quantity', width: 150 },
+  { field: 'buyinInitiatedLastUpdateDatetime', headerName: 'Buy-In Initiated Last Update DateTime', width: 175 },
+  { field: 'buyinCompleteId', headerName: 'Buy-In Complete Id', width: 175 },
+  { field: 'value', headerName: 'Value', width: 150 },
+  { field: 'currency', headerName: 'Currency', width: 150 },
+  { field: 'unit', headerName: 'Unit', width: 150 },
+  { field: 'valueDate', headerName: 'Value Date', width: 150 },
+  { field: 'buyinCompletedLastUpdateDatetime', headerName: 'Buy-In Completed Last Update DateTime', width: 175 }
+];
 
-function ReactTable({ columns, data }) {
-  const [columnFilters, setColumnFilters] = useState([]);
-  const [globalFilter, setGlobalFilter] = useState('');
-
-  const [pagination, setPagination] = useState({
-    pageIndex: 0,
-    pageSize: 4
-  });
-
-  const table = useReactTable({
-    data,
-    columns,
-    state: {
-      columnFilters,
-      globalFilter,
-      pagination
-    },
-    getCoreRowModel: getCoreRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    getFacetedRowModel: getFacetedRowModel(),
-    getFacetedUniqueValues: getFacetedUniqueValues(),
-    getFacetedMinMaxValues: getFacetedMinMaxValues(),
-    getPaginationRowModel: getPaginationRowModel(),
-    onColumnFiltersChange: setColumnFilters,
-    onGlobalFilterChange: setGlobalFilter,
-    onPaginationChange: setPagination
-  });
+function EnhancedTableToolbar(props) {
+  const { numSelected } = props;
 
   return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) => alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity)
+        })
+      }}
+    >
+      <Typography sx={{ flex: '1 1 100%' }} variant="h4" id="tableTitle" component="div">
+        Buy Ins
+      </Typography>
+    </Toolbar>
+  );
+}
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired
+};
+
+function ReactTable({ columns, rows }) {
+  return (
     <>
-      <MainCard content={false}>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <TableRow key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => (
-                    <TableCell key={header.id} {...header.column.columnDef.meta}>
-                      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))}
-            </TableHead>
-            <TableHead>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <TableRow key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => (
-                    <TableCell key={header.id} {...header.column.columnDef.meta}>
-                      {header.column.getCanFilter() && <Filter column={header.column} table={table} />}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))}
-            </TableHead>
-            <TableBody>
-              {table.getRowModel().rows.length > 0 ? (
-                table.getRowModel().rows.map((row) => (
-                  <TableRow key={row.id}>
-                    {row.getVisibleCells().map((cell) => (
-                      <TableCell key={cell.id} {...cell.column.columnDef.meta}>
-                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell colSpan={table.getAllColumns().length}>
-                    <EmptyTable msg="No Data" />
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </MainCard>
-      <div className="pt-2">
-        <Button
-          variant="contained"
-          startIcon={<KeyboardArrowLeftIcon />}
-          color="success"
-          size="small"
-          onClick={() => table.firstPage()}
-          disabled={!table.getCanPreviousPage()}
-        />
-        <Button
-          variant="contained"
-          startIcon={<KeyboardDoubleArrowLeftIcon />}
-          color="success"
-          size="small"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        />
-        <Button
-          variant="contained"
-          endIcon={<KeyboardArrowRightIcon />}
-          color="success"
-          size="small"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        />
-        <Button
-          variant="contained"
-          endIcon={<KeyboardDoubleArrowRightIcon />}
-          color="success"
-          size="small"
-          onClick={() => table.lastPage()}
-          disabled={!table.getCanNextPage()}
-        />
-        <div className="flex float-end">
-          Page:
-          {table.getState().pagination.pageIndex + 1} of {table.getPageCount().toLocaleString()}
+      <MainCard content={false} sx={{ width: '100%', overflow: 'hidden' }}>
+        <EnhancedTableToolbar numSelected={'0'} />
+        <div style={{ height: 675, width: '100%' }}>
+          <DataGrid
+            initialState={{
+              ...rows.initialState,
+              pagination: { paginationModel: { pageSize: 20 } }
+            }}
+            pageSizeOptions={[20, 50, 100]}
+            rows={rows}
+            columns={columns}
+          />
         </div>
-      </div>
+      </MainCard>
     </>
   );
 }
 
 ReactTable.propTypes = {
   columns: PropTypes.array,
-  data: PropTypes.array
+  rows: PropTypes.array
 };
-
-// ==============================|| REACT TABLE - EMPTY ||============================== //
 
 const BuyinsPage = () => {
   const [data, setData] = useState([]);
 
-  const columns = useMemo(
-    () => [
-      {
-        header: 'Buy-In Initiated Id',
-        accessorKey: 'buyinInitiatedId',
-        meta: {
-          className: 'cell-left'
-        }
-      },
-      {
-        header: 'Buy-In Date',
-        accessorKey: 'buyinDate'
-      },
-      {
-        header: 'Contract Id',
-        accessorKey: 'contractId'
-      },
-      {
-        header: 'Status',
-        accessorKey: 'status'
-      },
-      {
-        header: 'Open Quantity',
-        accessorKey: 'openQuantity'
-      },
-      {
-        header: 'Quantity',
-        accessorKey: 'quantity'
-      },
-      {
-        header: 'Buy-In Initiated Last Update DateTime',
-        accessorKey: 'buyinInitiateILastUpdateDatetime'
-      },
-      {
-        header: 'Buy-In Complete Id',
-        accessorKey: 'buyinCompleteId'
-      },
-      {
-        header: 'Value',
-        accessorKey: 'value'
-      },
-      {
-        header: 'Currency',
-        accessorKey: 'currency'
-      },
-      {
-        header: 'Unit',
-        accessorKey: 'unit'
-      },
-      {
-        header: 'Value Date',
-        accessorKey: 'valueDate'
-      },
-      {
-        header: 'Buy-In Completed Last Update Datetime',
-        accessorKey: 'buyinCompleteLastUpdateDatetime'
-      }
-    ],
-    []
-  );
-
   useEffect(() => {
-    const url = process.env.REACT_APP_TOOLKIT_API_URL + '/contracts';
+    const url = process.env.REACT_APP_TOOLKIT_API_URL + '/cloudevents';
     const token = localStorage.getItem('token');
 
     let respData = [];
@@ -246,15 +105,15 @@ const BuyinsPage = () => {
 
             respData.push(...nextPage.data.items);
           }
+
+          let vals = filterForDiscrepancies(respData);
+          setData(vals);
         }
       }
-
-      // Todo: set data from Splits Toolkit API
-      setData([]);
     })();
   }, []);
 
-  return <ReactTable columns={columns} data={data} />;
+  return <ReactTable columns={columns} rows={data} />;
 };
 
 export default BuyinsPage;
