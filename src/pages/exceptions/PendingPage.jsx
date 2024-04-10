948c201622975afa4369e877234f565566732a0a
Dharm Kapadia <dharm.kapadia@equilend.com>
2024-04-10 12:48:59 -0400

    Replace MUI Table with DataGrid on Pending page

--- src/pages/exceptions/PendingPage.jsx
+++ src/pages/exceptions/PendingPage.jsx
@@ -1,36 +1,20 @@
+import { Toolbar, Typography } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 import axios from 'axios';
 import PropTypes from 'prop-types';
-import * as React from 'react';
 import { useEffect, useState } from 'react';
-
-// material-ui
-import {
-  IconButton,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  TableRow,
-  Toolbar,
-  Tooltip,
-  Typography
-} from '@mui/material';
-
-import FilterListIcon from '@mui/icons-material/FilterList';
 
 // project import
 import MainCard from 'components/MainCard';
 import { filterForUnmatched } from 'utils/jsonHelper';
 
 const columns = [
-  { id: 'id', label: 'Event Id', minWidth: 150 },
-  { id: 'time', label: 'Event Time', minWidth: 150 },
-  { id: 'type', label: 'Event Type', minWidth: 100 },
-  { id: 'subject', label: 'Subject', minWidth: 100 },
-  { id: 'relatedprocess', label: 'Related Lifecycle Event', minWidth: 100 },
-  { id: 'message', label: 'Pending Details', minWidth: 125 }
+  { field: 'id', headerName: 'Event Id', width: 250 },
+  { field: 'time', headerName: 'Event Time', width: 200 },
+  { field: 'type', headerName: 'Event Type', width: 250 },
+  { field: 'subject', headerName: 'Subject', width: 150 },
+  { field: 'relatedprocess', headerName: 'Related Lifecycle Event', width: 200 },
+  { field: 'message', headerName: 'Discrepancy Details', width: 400 }
 ];
 
 function EnhancedTableToolbar(props) {
@@ -49,11 +33,6 @@ function EnhancedTableToolbar(props) {
       <Typography sx={{ flex: '1 1 100%' }} variant="h4" id="tableTitle" component="div">
         Pending
       </Typography>
-      <Tooltip title="Filter list">
-        <IconButton>
-          <FilterListIcon />
-        </IconButton>
-      </Tooltip>
     </Toolbar>
   );
 }
@@ -63,60 +42,13 @@ EnhancedTableToolbar.propTypes = {
 };
 
 function ReactTable({ columns, rows }) {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
   return (
     <>
       <MainCard content={false} sx={{ width: '100%', overflow: 'hidden' }}>
-        <TableContainer sx={{ maxHeight: 725 }}>
-          <EnhancedTableToolbar numSelected={0} />
-          <Table stickyHeader size="small" aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                {columns.map((column) => (
-                  <TableCell key={column.id} align="center" style={{ minWidth: column.minWidth }}>
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number' ? column.format(value) : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
+        <EnhancedTableToolbar numSelected={'0'} />
+        <div style={{ height: 675, width: '100%' }}>
+          <DataGrid rows={rows} columns={columns} />
+        </div>
       </MainCard>
     </>
   );
