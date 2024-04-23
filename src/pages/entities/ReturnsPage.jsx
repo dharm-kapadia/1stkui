46f560ce0620e98e199ee34fe1f3d4c9e8f019d3
Dharm Kapadia <dharm.kapadia@equilend.com>
2024-04-23 16:21:41 -0400

    Refactor Returns page to support refresh of table data

--- src/pages/entities/ReturnsPage.jsx
+++ src/pages/entities/ReturnsPage.jsx
@@ -1,16 +1,16 @@
-import { IconButton, Stack, Toolbar, Typography } from '@mui/material';
+import { IconButton, Stack, Toolbar, Tooltip, Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
-import axios from 'axios';
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 
-import KeyboardReturnOutlinedIcon from '@mui/icons-material/KeyboardReturnOutlined';
+import UpdateIcon from '@mui/icons-material/Update';
 
 import Box from '@mui/material/Box';
 import MainCard from 'components/MainCard';
+import { getReturns } from 'services/returns';
 
 const columns = [
-  { field: 'returnId', headerName: 'Return Id', width: 175, headerAlign: 'center', headerClassName: 'super-app-theme--header' },
+  { field: 'id', headerName: 'Return Id', width: 175, headerAlign: 'center', headerClassName: 'super-app-theme--header' },
   { field: 'contractId', headerName: 'Contract Id', width: 175, headerAlign: 'center', headerClassName: 'super-app-theme--header' },
   { field: 'quantity', headerName: 'Quantity', width: 150, headerAlign: 'center', headerClassName: 'super-app-theme--header' },
   { field: 'returnDate', headerName: 'Return Date', width: 175, headerAlign: 'center', headerClassName: 'super-app-theme--header' },
@@ -60,6 +60,10 @@ const columns = [
 function EnhancedTableToolbar(props) {
   const { numSelected } = props;
 
+  function refreshReturnsData() {
+    props.onChange();
+  }
+
   return (
     <Toolbar
       sx={{
@@ -71,26 +75,57 @@ function EnhancedTableToolbar(props) {
       }}
     >
       <Stack direction="row" spacing={0.5} alignItems="center">
-        <IconButton color="primary" aria-label="Returns" size="medium">
-          <KeyboardReturnOutlinedIcon />
-        </IconButton>
         <Typography sx={{ flex: '1 1 100%' }} variant="h3" id="tableTitle" component="div">
           Returns
         </Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}> </Box>
+        <Tooltip title="Refresh data">
+          <IconButton
+            sx={{ flex: '1 1' }}
+            color="primary"
+            onClick={() => {
+              refreshReturnsData();
+            }}
+          >
+            <UpdateIcon />
+          </IconButton>
+        </Tooltip>
       </Stack>
     </Toolbar>
   );
 }
 
 EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired
+  numSelected: PropTypes.number.isRequired,
+  onChange: PropTypes.func
 };
 
-function ReactTable({ columns, rows, loading }) {
+const ReturnsPage = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function getReturnsData() {
+    setLoading(true);
+
+    let declined = await getReturns();
+    setData(declined);
+
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getReturnsData();
+  }, []);
+
   return (
     <>
       <MainCard content={false} sx={{ width: '100%', overflow: 'hidden' }}>
-        <EnhancedTableToolbar numSelected={0} />
+        <EnhancedTableToolbar
+          numSelected={0}
+          onChange={() => {
+            getReturnsData();
+          }}
+        />
         <Box
           sx={{
             height: 675,
@@ -113,14 +148,14 @@ function ReactTable({ columns, rows, loading }) {
               }
             }}
             initialState={{
-              ...rows.initialState,
+              ...data.initialState,
               pagination: { paginationModel: { pageSize: 20 } },
               sorting: {
                 sortModel: [{ field: 'returnDate', sort: 'desc' }]
               }
             }}
             pageSizeOptions={[20, 50, 100]}
-            rows={rows}
+            rows={data}
             columns={columns}
             loading={loading}
           />
@@ -128,58 +163,6 @@ function ReactTable({ columns, rows, loading }) {
       </MainCard>
     </>
   );
-}
-
-ReactTable.propTypes = {
-  columns: PropTypes.array,
-  rows: PropTypes.array,
-  loading: PropTypes.bool
-};
-
-const ReturnsPage = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const url = localStorage.getItem('url') + '/cloudevents';
-    const token = localStorage.getItem('token');
-
-    let respData = [];
-
-    // Get cloudevents using Bearer token
-    (async () => {
-      setLoading(true);
-
-      const result = await axios.get(url, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-
-      if (result.data.totalItems !== 0) {
-        respData = result.data.items;
-
-        if (result.data.totalPages > 1) {
-          // Make multiple calls to get full dataset
-          for (let i = 1; i < result.data.totalPages; i++) {
-            const nextPage = await axios.get(url + `?page=${i}`, {
-              headers: {
-                Authorization: `Bearer ${token}`
-              }
-            });
-
-            respData.push(...nextPage.data.items);
-          }
-        }
-      } else {
-        setData(respData);
-      }
-
-      setLoading(false);
-    })();
-  }, []);
-
-  return <ReactTable columns={columns} rows={data} loading={loading} />;
 };
 
 export default ReturnsPage;
---
+++ src/services/returns.jsx
@@ -0,0 +1,64 @@
+import axios from 'axios';
+
+// Iterate through the API call response and search for only return events
+export const filterForReturns = (input) => {
+  var returns = [];
+
+  input.forEach((item) => {
+    if (item.type.includes('RETURNS')) {
+      var obj = {};
+
+      obj['id'] = item.returnId;
+      obj['contractId'] = item.contractId;
+      obj['quantity'] = item.quantity;
+      obj['returnDate'] = item.returnDate;
+      obj['status'] = item.status;
+      obj['partyId'] = item.partyId;
+      obj['returnType'] = item.returnType;
+      obj['venueName'] = item.venueName;
+      obj['venueRefKey'] = item.venueRefKey;
+      obj['transactionDatetime'] = item.transactionDatetime.replace('T', ' ').substring(0, 19);
+      obj['partyRole'] = item.partyRole;
+      obj['venueRefPartyKey'] = item.venueRefPartyKey;
+      obj['localVenueFieldName'] = item.localVenueFieldName;
+      obj['localVenueFieldValue'] = item.localVenueFieldValue;
+      obj['lastUpdateDatetime'] = item.lastUpdateDatetime.replace('T', ' ').substring(0, 19);
+
+      declined.push(obj);
+    }
+  });
+
+  return returns;
+};
+
+export const getReturns = async () => {
+  const url = localStorage.getItem('url') + '/cloudevents';
+  const token = localStorage.getItem('token');
+
+  let respData = [];
+
+  const result = await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  if (result.data.totalItems !== 0) {
+    respData = result.data.items;
+
+    if (result.data.totalPages > 1) {
+      // Make multiple calls to get full dataset
+      for (let i = 1; i < result.data.totalPages; i++) {
+        const nextPage = await axios.get(url + `?page=${i}`, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+
+        respData.push(...nextPage.data.items);
+      }
+    }
+
+    return filterForReturns(respData);
+  }
+};
